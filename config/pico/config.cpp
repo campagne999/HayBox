ba38f4a7009d40fcdbfa1f44518232b4a9433854
campagne999 <78290733+campagne999@users.noreply.github.com>
2024-04-25 19:43:30 -0500

    Update config.cpp
    change default to Ultimate

--- config/pico/config.cpp
+++ config/pico/config.cpp
@@ -129,9 +129,9 @@ void setup() {
         backends = new CommunicationBackend *[backend_count] { primary_backend };
     }
 
-    // Default to Melee mode.
+    // Default to Ultimate mode.
     primary_backend->SetGameMode(
-        new Melee20Button(socd::SOCD_2IP_NO_REAC, { .crouch_walk_os = false })
+        new Ultimate(socd::SOCD_2IP)
     );
 }
 
